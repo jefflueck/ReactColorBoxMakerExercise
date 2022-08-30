67cc78af8b534679e815bd8e68def208f18f4146
jefflueck <lueckcfd@yahoo.com>
2022-08-30 12:28:43 -0500

    added tests: smoke test, snapshot, functional tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
+import BoxList from './BoxList';
+import NewBoxForm from './NewBoxForm';
+import Box from './Box';
 
-test('renders learn react link', () => {
+test('it renders the App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+test('it renders a Box List Component', () => {
+  render(<BoxList />);
+  render(<NewBoxForm />);
+});
+
+test('it matches snapshot for Box in BoxList', () => {
+  const { asFragment } = render(
+    <Box background-color="red" width="200px" height="200px" />
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('it should add a new box', () => {
+  const { queryByText, getByLabelText } = render(<BoxList />);
+  const input = getByLabelText('Color:');
+  const width = getByLabelText('Width:');
+  const height = getByLabelText('Height:');
+  const btn = queryByText('Add Box');
+  fireEvent.change(input, width, height, {
+    target: { backgroundColor: 'red', width: '200px', height: '200px' },
+  });
+  fireEvent.click(btn);
+  expect(<Box background-color={'red'} width={'200px'} height={'200px'} />);
 });
